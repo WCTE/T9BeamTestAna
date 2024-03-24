17dd437ee33ab3a3ecd338a3c68b6f019c7d531d
Jiri Kvita <jiri.kvita@gmail.com>
2024-03-24 22:16:54 +0100

    multifit

---
+++ python/BetheBlochMultiFit.py
@@ -0,0 +1,442 @@
+#!/snap/bin/pyroot
+
+#/usr/bin/python3
+
+# jk
+# 20/09/2022
+# 14.7.2023
+#  5.3.2024
+#  6.3.2024
+# 24.3.2024
+
+#from __future__ import print_function
+
+import ROOT
+from math import sqrt, pow, log, exp
+import os, sys, getopt
+
+from labelTools import *
+from tofUtil import *
+from FitTools import *
+
+
+stuff = []
+lines = []
+
+####################################################################################
+
+def makeLine(x1, x2, y1, y2):
+    line = ROOT.TLine(x1, y1, x2, y2)
+    line.SetLineColor(ROOT.kGreen)
+    line.SetLineWidth(2)
+    line.Draw()
+    return line
+
+####################################################################################
+
+def PrintUsage(argv):
+    print('Usage:')
+    print('{} filename_plots.root [-b]'.format(argv[0]))
+    print('Example:')
+    print('{} output_300n_plots.root -b'.format(argv[0]))
+    return
+####################################################################################
+
+def MakeGraph(xs, exs, ys, eys, col = ROOT.kBlack, mst = 20, ms = 1.):
+    gr = ROOT.TGraphErrors()
+    for i in range(0,len(xs)):
+        gr.SetPoint(i, xs[i], ys[i])
+        gr.SetPointError(i, exs[i], eys[i])
+    gr.SetMarkerColor(col)
+    gr.SetMarkerStyle(mst)
+    gr.SetMarkerSize(ms)
+    return gr
+
+####################################################################################
+def readInputFiles():
+    dirname = 'histos/windowpe_analyzed/'
+    filenames = [
+        'peakAnalysed_timeCorr_windInt_-16_45_000403_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000396_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000394_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000393_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000392_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000398_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000399_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000402_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000449_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000436_plots_f.root',
+        'peakAnalysed_timeCorr_windInt_-16_45_000435_plots_f.root'
+    ]
+    rfiles = []
+    for filename in filenames:
+        rfile = ROOT.TFile(dirname + filename, 'read')
+        if not rfile.IsZombie():
+            rfiles.append(rfile)
+    return rfiles
+
+####################################################################################
+# https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+def singleFit(argv, rfiles, TStag, particle):
+    #if len(sys.argv) > 1:
+    #  foo = sys.argv[1]
+    cans = []
+
+    ROOT.gStyle.SetOptFit(111)
+    print('*** Settings:')
+
+    bgmin = 0.4
+    bgmax = 1.5
+    bmin = 0.4
+    bmax = 0.85
+    
+    #scint1 = 4.
+    #scint2 = 22.
+    scint1 = 0.
+    scint2 = 2500.
+    t1 = 13.
+    t2 = 35.
+    if particle == 'D':
+        t2 = 60.
+        bgmin = 0.4
+        bgmax = 0.7
+        bmin = 0.4
+        bmax = 0.6
+
+    if particle == 'T':
+        t2 = 85.
+        bgmin = 0.2
+        bgmax = 0.7
+        bmin = 0.2
+        bmax = 0.5
+   
+   
+    os.system('mkdir -p pdf png')
+    hs = []
+    momenta = []
+    hscp = []
+    projYs = []
+    txts = []
+    #basedir = 'TrigScint/'
+
+    tstag = 'TS' + TStag
+    if tstag == 'TS':
+        tstag = 'bothTS'
+    
+    pbasedir = 'TrigScint_{}/'.format(particle)
+    hname = 'hRef_TOF_TrigScint{}C_{}-like'.format(TStag, particle)
+    
+    can = None
+    opt = ''
+    ys = []
+    eys = []
+    betas = []
+    betagammas = []
+    ebetas = []
+    leg = ROOT.TLegend(0.12, 0.65, 0.40, 0.88)
+    leg.SetBorderSize(0)
+    stuff.append(leg)
+    cols = [ROOT.kBlack, ROOT.kGreen+2, ROOT.kBlue, ROOT.kViolet, ROOT.kRed,
+            ROOT.kOrange+1, ROOT.kYellow, ROOT.kYellow+2, ROOT.kCyan+1, ROOT.kTeal-7,
+            ROOT.kAzure, ROOT.kGray+2, ROOT.kGreen+1, ROOT.kBlue, ROOT.kMagenta]
+    for rfile,col in zip(rfiles,cols):
+        filename = rfile.GetName()
+        momentum = None
+        runindex = -1;
+        srun = ''
+        try:
+            runindex = filename.index('run')
+            srun = filename[runindex+6:runindex+9]
+        except:
+            runindex = filename.index('000')
+            srun = filename[runindex+3:runindex+6]
+        if momentum == None:
+            momentum = getMomentum(srun)
+        if momentum == None:
+            momentum = getMergedMomentum(srun)
+        print(srun,momentum)
+        momenta.append(momentum)
+        Hs = []
+        Txts = []
+        ftag = filename.split('/')[-1].replace('output_','').replace('_plots_f.root','').replace('_plots.root','')
+
+        h = rfile.Get(pbasedir + hname)
+        try:
+            print('ok, got {} from file {}'.format(h.GetName(), rfile.GetName()))
+            tmp = h.GetName()
+            #print('meanX: {1.2f}'.format(h.GetMean(1)))
+            #print('meanY: {1.2f}'.format(h.GetMean(2)))
+        except:
+            print('ERROR getting histo {}{} from file {}!'.format(pbasedir,hname, rfile.GetName()))
+            continue
+
+        hs.append(h)
+        if can == None:
+            canname = 'WCTEJuly2023_BetheBloch_SingleFit_{}_{}'.format(particle, tstag)
+            canname = canname.replace('_list_root','').replace('_ntuple','')
+            can = ROOT.TCanvas(canname, canname, 0, 0, 1100, 800)
+            cans.append(can)
+            #can.Divide(8,4)
+        h.SetStats(0)
+
+        projY = h.ProjectionY()
+        projYs.append(projY)
+        if particle == 'D' or particle == 'T':
+            #h.Rebin2D(2,2)
+            projY.Rebin(4)
+        
+        hcp = h.DrawCopy('scat' + opt)
+        if opt == '':
+            hcp.GetXaxis().SetRangeUser(t1, t2)
+            hcp.GetYaxis().SetRangeUser(scint1, scint2)
+        alpha = 0.10
+        hcp.SetMarkerColorAlpha(col, alpha)
+        hcp.SetLineColorAlpha(col, alpha)
+        hcp.SetMarkerSize(0.1)
+        hcp.SetMarkerStyle(6)
+        ROOT.gPad.Update()
+        h.SetFillColorAlpha(hcp.GetMarkerColor(),1)
+        h.SetFillStyle(1111)
+        h.SetMarkerSize(2)
+        h.SetMarkerStyle(20)
+        hscp.append(hcp)
+        beta = getBeta(ms[particle], momentum)
+        betagamma = getBetaGamma(ms[particle], momentum)
+        leg.AddEntry(h, 'p = {:4} MeV/c #beta={:1.2f}'.format(str(momentum), beta), 'F')
+
+
+        projYcp = projY.Clone(projY.GetName() + '_cp')
+
+        meanfull = projY.GetMean()
+        sigmafull = projY.GetStdDev()
+
+        x2 = projY.GetXaxis().GetXmax()
+        x1 = max(projY.GetXaxis().GetXmin(), meanfull - sigmafull)
+        projYcp.GetXaxis().SetRangeUser(x1, x2)
+        if projY.GetEntries() > 10.:
+            mean = projYcp.GetMean()
+            meanerr = projYcp.GetMeanError()        
+            ys.append(mean)
+            eys.append(meanerr)
+            betas.append(beta)
+            betagammas.append( betagamma)
+            ebetas.append(0.)
+
+        opt = 'same'
+        #adjustStats(h)
+        #ROOT.gPad.Update()
+
+    leg.Draw()
+    cnote, pnote = makePaperLabel(srun, momentum, 0.12, 0.92)
+    #cnote.Draw()
+    #pnote.Draw()
+    pnote2 = makeMomentumLabel(srun, momentum, 0.12, 0.92)
+    #pnote2.Draw()
+    #parts = ['e', 'mu', 'pi', 'K', 'p', 'D', 'T']
+    #lines = makeLines(h, 0., parts, momentum, True)
+    #stuff.append(lines)
+    #stuff.append([cnote, pnote, pnote2])
+
+    ####################################################################################
+    canname = 'WCTEJuly2023_LandauProfiles_{}_{}'.format(particle, tstag)
+    canp = ROOT.TCanvas(canname, canname, 200, 200, 1000, 800)
+    canp.cd()
+    opt = ''
+    legp = ROOT.TLegend(0.65, 0.65, 0.88, 0.88)
+    legp.SetBorderSize(0)
+    stuff.append(legp)
+    for h,projY,momentum,beta in zip(hs,projYs,momenta,betas):
+        projY.SetLineColor(h.GetFillColor())
+        projY.SetFillColorAlpha(h.GetFillColor(), 0.3)
+        projY.SetLineWidth(2)
+        projY.SetLineStyle(1)
+        projY.Rebin(2)
+        val = projY.Integral(0, projY.GetXaxis().GetNbins()+1)
+        if val > 0.:
+            projY.Scale(1./val)
+        projY.SetMaximum(projY.GetMaximum()*1.2)
+        projY.SetStats(0)
+        projY.Draw('hist' + opt)
+        if opt == '':
+            projY.GetXaxis().SetRangeUser(scint1, scint2)
+        legp.AddEntry(projY, 'p = {:4} MeV/c #beta={:1.2f}'.format(str(momentum), beta), 'F')
+        opt = 'same'
+    legp.Draw()
+    canp.Update()
+    cans.append(canp)
+
+    
+    ####################################################################################
+    canname = 'BetaGraph_{}_{}'.format(particle, tstag)
+    gcan = ROOT.TCanvas(canname, canname, 100, 100, 1200, 600)
+    gcan.Divide(2,1)
+    print(betas, ebetas, ys, eys)
+    grb = MakeGraph(betas, ebetas, ys, eys)
+    grbg = MakeGraph(betagammas, ebetas, ys, eys)
+    
+    hn = 'tmpbg' + tstag + particle
+    ht = hn + ';#beta#gamma;Mean trig. scint. charge [a.u.];'
+    htmpbg = ROOT.TH2D(hn, ht, 100, bgmin, bgmax, 100, scint1, scint2)
+    htmpbg.SetStats(0)
+    htmpbg.GetXaxis().SetMoreLogLabels()
+
+    gcan.cd(1)
+    htmpbg.Draw()
+    ROOT.gStyle.SetOptTitle(0)
+    #ROOT.gPad.SetLogx()
+    ROOT.gPad.SetGridx(1)
+    ROOT.gPad.SetGridy(1)
+    grbg.Draw("P")
+
+    hn = 'tmpbg' + tstag + particle
+    ht = hn + ';#beta;Mean trig. scint. charge [a.u.];'
+    htmpb = ROOT.TH2D(hn, ht, 100,bmin, bmax, 100, scint1, scint2)
+    htmpb.SetStats(0)
+    htmpb.GetXaxis().SetMoreLogLabels()
+
+    gcan.cd(2)
+    htmpb.Draw()
+    ROOT.gStyle.SetOptTitle(0)
+    #ROOT.gPad.SetLogx()
+    ROOT.gPad.SetGridx(1)
+    ROOT.gPad.SetGridy(1)
+    grb.Draw("P")
+    #fun = ROOT.TF1('fun', '[0]/x^2 + [1]', 0.1, 1.)
+    #fun.SetParameters(0.1, 1.)
+    #fun = ROOT.TF1('fun', '[0]/x^2*(log([1]*x/sqrt(1-x*x)) - x^2) + [2]', 0.1, 1.)
+    #fun.SetParameters(2., 10., 0.5)
+    fun = ROOT.TF1('fun', '[0]/x^2*(log([1]*x/sqrt(1-x*x)) - x^2)', 0.1, 1.)
+    fun.SetParameters(2., 10.)#, 0.5)
+    fun.SetParName(0, 'a')
+    fun.SetParName(1, 'b')
+    #fun.SetParName(2, 'g')
+    grb.Fit('fun')
+    
+    cans.append(gcan)
+    stuff.append([grb, grbg])
+    gcan.Update()
+
+    
+    return grb, grbg, cans, projYs
+
+
+
+###################################
+###################################
+###################################
+
+def main(argv):
+
+    # execute only if run as a script"
+    gBatch = False
+    gTag=''
+
+    pngdir = 'png_results/'
+    pdfdir = 'pdf_results/'
+    os.system(f'mkdir {pngdir}')
+    os.system(f'mkdir {pdfdir}')
+
+  
+
+    ### https://www.tutorialspoint.com/python/python_command_line_arguments.htm
+    ### https://pymotw.com/2/getopt/
+    ### https://docs.python.org/3.1/library/getopt.html
+    print(argv[1:])
+    try:
+        # options that require an argument should be followed by a colon (:).
+        opts, args = getopt.getopt(argv[2:], 'hbt:', ['help','batch','tag='])
+        print('Got options:')
+        print(opts)
+        print(args)
+    except getopt.GetoptError:
+        print('Parsing...')
+        print ('Command line argument error!')
+        print('{:} [ -h -b --batch -tTag --tag="MyCoolTag"]]'.format(argv[0]))
+        sys.exit(2)
+    for opt,arg in opts:
+        print('Processing command line option {} {}'.format(opt,arg))
+        if opt == '-h':
+            print('{:} [ -h -b --batch -tTag --tag="MyCoolTag"]'.format(argv[0]))
+            sys.exit()
+        elif opt in ("-b", "--batch"):
+            gBatch = True
+            print('OK, running in batch mode')
+        elif opt in ("-t", "--tag"):
+            gTag = arg
+            print('OK, using user-defined histograms tag for output pngs {:}'.format(gTag,) )
+
+    if gBatch:
+        ROOT.gROOT.SetBatch(1)
+
+    if len(argv) < 1:
+        PrintUsage(argv)
+        return
+
+    #ROOT.gStyle.SetPalette(ROOT.kSolar)
+    ROOT.gStyle.SetPalette(ROOT.kRainBow)
+    #ROOT.gStyle.SetPalette(1)
+
+    #TStag = ''
+    TStags = ['0',
+              '1'
+              #'' # both trigger scintillators
+              ]
+    particles = [ 'p', # protons
+                  'D', # deuterons
+                  #'T' # tritium
+                 ]
+    rfiles = readInputFiles()
+    # graphs of E losses in p.e. as function of beta or beta*gamma
+    GrsBeta = {}
+    GrsBg = {}
+    Cans = []
+    for TStag in TStags:
+        for particle in particles:
+            region = 'TS' + TStag + particle
+            grbeta, grbg, cans, projYs = singleFit(sys.argv, rfiles, TStag, particle)
+            GrsBeta[region] = grbeta
+            stuff.append([grbeta, grbg, cans, projYs])
+            Cans.append(cans)
+    
+    step = 0.01
+    pars, parerrs = doTheFit(GrsBeta, step)
+
+    # TODO: analyze the fitter parameters
+    
+    # plot individual subfits over data in each region!
+
+    for cans in Cans:
+        for can in cans:
+            can.cd()
+            if 'vs' in can.GetName():
+                pnote.Draw()            
+            can.Update()
+            can.Print(pngdir + can.GetName() + '.png')
+            can.Print(pdfdir + can.GetName() + '.pdf')
+    
+    if not gBatch:
+        ROOT.gApplication.Run()
+
+    
+    print('DONE!')
+    
+###################################
+###################################
+###################################
+
+
+
+
+
+###################################
+###################################
+###################################
+
+if __name__ == "__main__":
+    # execute only if run as a script"
+    main(sys.argv)
+    
+###################################
+###################################
+###################################
+
--- python/FitTools.py
+++ python/FitTools.py
@@ -1,134 +1,329 @@
 #!/usr/bin/python
 
 # jk 24.8.2021
+# jk 24.3.2024
 
 import ROOT
 
-from math import log, exp, pow
+from math import log, exp, pow, sqrt
+from ctypes import c_double
+from array import array
 
-kBadMu = -0.1
+from tofUtil import ms
+
 kBadP0 = -1
 kEpsilon = 1.e-5
 
-gsigVals = []
-gbgVals = []
-gdataVals = []
+#gPars = []
+gInitPars = {}
+gDataPoints = {}
 
 # number of studied variables = free parameters
-gN = 1
+#gN = -1
 
-#################################
-def getPoissLog(k, nu):
-    plog = 0.
-    # nu is the expected=true
-    # k is the data
-    # P(nu,k) = exp(-nu) nu^k / k!
-    if nu <= 0.:
+
+# region names:
+gTS0p = 'TS0p'
+gTS0D = 'TS0D'
+gTS0T = 'TS0T'
+gTS1p = 'TS1p'
+gTS1D = 'TS1D'
+gTS1T = 'TS1T'
+
+# not preferred:
+gTS1p = 'TSboth_p'
+gTS1D = 'TSboth_D'
+gTS1T = 'TSboth_T'
+
+
+# https://root.cern/manual/python/#alternative-for-tpymultigenfunction-and-tpymultigradfunction
+
+
+##################################################################
+def initGlobalPars():
+    gInitPars['A'] = [1.e-4, 1e4]
+    gInitPars['B'] = [1.e-4, 10.]
+    gInitPars['C'] = [0., 3000.]
+    return
+
+##################################################################
+class cPoint:
+    def __init__(self,x,y,ey):
+        self.x = x
+        self.y = y
+        self.ey = ey
+
+##################################################################
+# expect a dictionary of dE/dX graphs in fit regions
+def prepareData(grs):
+    print('Preparing fit data')
+    for region in grs:
+        gDataPoints[region] = []
+    x = c_double(0.)
+    y = c_double(0.)
+        
+    for region,gr in grs.items():
+        print(f'  processing region {region}')
+        for ip in range(0,gr.GetN()):
+            gr.GetPoint(ip, x, y)
+            ey = gr.GetErrorY(ip)
+            gDataPoints[region].append(cPoint(x.value, y.value, 1.*ey))
+
+    # basic one region fit, parameters A, B
+    npars = 2
+    if len(grs) > 2: # expect we fit split of TS0 and TS1 for one or more particle types
+        npars = 3
+    print('  initialized {} graph regions'.format(len(grs)))
+    return npars
+
+##################################################################
+def getBaseFit(beta, A, B, debug = 0):
+    fval = 0.
+    if beta > 0. and beta <= 1.:
+        # Bethe-Bloch ansatz:
+        fval = A / pow(beta,2) * (log(B*beta/sqrt(1-pow(beta,2))) - pow(beta,2))
+    else:
+        print('ERROR in getBaseFit: beta out of physically allowed range!')
+    if debug: print(f'fval={fval}')
+    return fval
+
+##################################################################
+def getFitVal(region, x, npars, pars, debug = 0):
+    #A = gPars[0]
+    #B = gPars[1]
+    A = pars[0]
+    B = pars[1]
+    beta = x
+    # dEdX congversion par
+    C = 0.
+    particle = ''
+    # names: Trigger Scintillator TS 0 or 1
+    # particles p, D, T:
+    if npars > 2 and (region == gTS0p or region == gTS0D or region == gTS0T):
+        C = pars[2]
+        if 'p' == region[-1]:
+            particle = 'p'
+        elif 'D' == region[-1]:
+            particle = 'D'
+        elif 'T' == region[-1]:
+            particle = 'T'
+        if debug: print(f'particle: "{particle}"')
+        m = 0.
+        try:
+            m = ms[particle]
+        except:
+            print('Failed getting particle mass and correct the beta!')
+        if debug: print(f'm={m} A={A} B={B} C={C}')
+        if m > 0.:
+            dE = C * getBaseFit(x, A, B, debug)
+            if dE > 0:
+                gamma = 1./sqrt(1. - pow(beta,2))
+                E = m*gamma
+                gamma0 = (E + dE) / m
+                if debug: print(f'x={x} E={E} dE={dE} gamma0={gamma0}')
+                if gamma0 > 1.:
+                    beta = sqrt( 1. - 1./pow(gamma0,2))
+            else:
+                if debug:
+                    print('ERROR, negative energy correction!')
+        
+    fval = getBaseFit(beta, A, B, debug)
+    return fval
+
+##################################################################
+def getChi2Term(region, dpoint, npars, pars, debug):
+    term = 0.
+    x = dpoint.x
+    y = dpoint.y
+    ey = dpoint.ey
+    if ey <= 0.:
         return 0.
-    plog = -nu + k*log(nu) # drop the constant data-dependent term (data is given!): -log(factorial(k))
-    return plog
+    fitVal = getFitVal(region, x, npars, pars, debug)
+    term = pow( (y - fitVal)/ey , 2) 
+    return term
 
-#################################
-def getNegLogLhood(mu):
-    lhoodLog = 0.
-    for i in range(0, len(gsigVals)):
-        lhoodLog = lhoodLog - getPoissLog(gdataVals[i], mu*gsigVals[i] + gbgVals[i])
-    return lhoodLog
+##################################################################
+#def getChi2(n, grad, x, par):
+def getChi2(npars, pars, debug):
+    chi2 = 0.
+    for region in gDataPoints:
+        for dpoint in gDataPoints[region]:
+            chi2 = chi2 + getChi2Term(region, dpoint, npars, pars, debug)
+    return chi2
 
-
-#################################
+##################################################################
 # for Minuit:
 # https://root-forum.cern.ch/t/numerical-minimization-with-root-math-functor/13286/2
+#class MyFunction( ROOT.Math.IMultiGenFunction ):
+class MyFunction( object ):
+    
+    # lass MyFunction( ROOT.Fit.Fitter.MinuitFCN_t ):
+    # defint init with npars passed?
+    def __init__(self, npars, debug):
+        self.npars = npars
+        self.debug = debug
+    
+    #def NDim( self ):
+    #    print('PYTHON NDim called')
+    #    return self.npars
 
-class MyFunction( ROOT.TPyMultiGenFunction ):
-    def __init__( self ):
-        print("CREATED")
-        ROOT.TPyMultiGenFunction.__init__( self, self )
+    #def DoEval(self, x):
+    def __call__(self, pars):
+        # set the global parameters based on the actual args called
+        #for i in range(0, self.NDim()):
+        #    gPars[i] = x[i]
+        chi2 = getChi2(self.npars, pars, self.debug)
+        if self.debug: print(f'chi2={chi2}')
+        return chi2
 
-    def NDim( self ):
-        print('PYTHON NDim called')
-        return gN
+    #def Clone( self ):
+    #    x = MyMultiGenFCN()
+    #    ROOT.SetOwnership(x, False)
+    #    return x
+##################################################################
+def mySimpleFcn(npars, x):
+    # set the global parameters based on the actual args called
+    #for i in range(0, npars):
+    #    gPars[i] = x[i]
+    chi2 = getChi2(npars, x)
+    return chi2
 
-    def DoEval(self, args):
-        lhood = 0.
-        # TO CHECK!!!
-        mu = args[0]
-        negloglhood = getNegLogLhood(mu)
-        return negloglhood
 
-#################################
-def minimizeLhood(step, mumin, mumax):
+##################################################################
+def minimizeChi2(npars, step = 0.01):
 
-    mu = 0.5 * (mumax + mumin)
-    muerr = kBadMu
     # https://root-forum.cern.ch/t/numerical-minimization-with-root-math-functor/13286#p58684
 
     # not returning errors:
     # minimizer = ROOT.Math.Factory.CreateMinimizer("GSLMultiMin", "BFGS")
+    # OLD:
+    """
     minimizer = ROOT.Math.Factory.CreateMinimizer('Minuit2', 'Minuit2' )
     minimizer.SetMaxFunctionCalls(1000000)
     minimizer.SetMaxIterations(100000)
     minimizer.SetTolerance(0.001)
     minimizer.SetPrintLevel(1)
-    
-    myfun = MyFunction()
+    """
+    # OLD: myfun = MyFunction()
     # https://root.cern.ch/doc/master/classTMinuitMinimizer.html#a069cdb1a4cf6e2f373832a4cb094c6d2
-    minimizer.SetLimitedVariable(0, "mu", mu, step, mumin, mumax)
-    minimizer.SetFunction(myfun)
 
-    # minimize the -log(L)!;-)
-    minimizer.Minimize()
-    status = minimizer.Status()
+    # NEW:
+    fitter = ROOT.Fit.Fitter()
+    
+    pars = []
+    # ipar in range(0,npars):
+    ipar = -1
+    for parName,parLimits in gInitPars.items():
+        ipar = ipar + 1
+        if ipar >= npars:
+            break
+        # set initial vals to the mean of limits:
+        par = (parLimits[1] - parLimits[0]) / 2.
+        print(f'Setting initial par {par}')
+        pars.append(par)
+        # OLD:
+        # minimizer.SetLimitedVariable(0, parName, par, step, parLimits[0], parLimits[1])
+        #fitter.SetLimitedVariable(0, parName, par, step, parLimits[0], parLimits[1])
 
-    # get the minimized parameter:
-    if status == 0:
-        print('minimizeLhood :: SUCCESSFUL MINIMIZATION! ;-)')
-        mu =  minimizer.X()[0]
-        muerr = minimizer.Errors()[0]
-        #print('minimizeLhood :: Lhood fit result: mu={} +/- {}'.format(mu, muerr))
-    else:
-        print('minimizeLhood :: FAILED MINIMIZATION! :-(')
-        mu = kBadMu
-    return mu, muerr
+    params = array('d', pars)
+
+    # See:
+    # $ROOTSYS/tutorials/fit/combinedFit.py
+    #fitter.Config().MinimizerOptions().SetPrintLevel(0)
+
+    fitter.Config().SetMinimizer("Minuit2", "Migrad")
+    npars = len(params)
+    debug = 0
+    myfcn = MyFunction(npars, debug)
+
+    # OLD:
+    #minimizer.SetFunction(myfun)
+    #minimizer.Minimize()
+    #status = minimizer.Status()
+
+    #fitter.FitFCN(myfcn)
+    # NEW:
+    print(f'Fit parameters: {npars}')
+    # we can't pass the Python object globalChi2 directly to FitFCN.
+    # It needs to be wrapped in a ROOT::Math::Functor.
+    globalChi2Functor = ROOT.Math.Functor(myfcn, npars)
+    #fitter.FitFCN(globalChi2Functor)
+    dataSize = len(gDataPoints)
+
+    print('Setting parameters limits')
+    fitter.Config().SetParamsSettings(npars, params)
+    ipar = -1
+    for parName,parLimits in gInitPars.items():
+        ipar = ipar + 1
+        if ipar >= npars:
+            break
+        fitter.Config().ParSettings(ipar).SetLimits(parLimits[0], parLimits[1])
+    
+    fitter.FitFCN(globalChi2Functor) #, 0, dataSize, True)
+
+    # TO TRY:
+    # fitter.Config().ParSettings(0).SetName("Parameter0").SetLimits(-10, 10)
+
+    # back to minimizer...or not
+    #minimizer = ROOT.Math.Minimizer(ROOT.Math.MinimizerOptions.DefaultMinimizerType())
+    #minimizer.SetMaxFunctionCalls(10000)
+    #minimizer.SetMaxIterations(1000)
+    #minimizer.SetTolerance(0.001)
+    #minimizer.SetFunction(myfcn)
+    #minimizer.Minimize()
+    
+    #fitter.FitFCN(mySimpleFcn, len(params), params)
+    #status = fitter.Result()
+    # status = fitter.Result()
+    #.Print(ROOT.std.cout, True)
+
+ 
+    result = fitter.Result()
+    result.Print(ROOT.std.cout)
+
+    pars = []
+    parerrs = []
+    ## get the minimized parameter:
+    #if status == 0:
+    #    print('minimizeChi2 :: SUCCESSFUL MINIMIZATION! ;-)')
+    #    ipar = -1
+    #    Xs =  minimizer.X()
+    #    Errs = minimizer.Errors()
+    #    for parname in gInitPars:
+    #        ipar = ipar+1
+    #        par =  Xs[ipar]
+    #        parerr = Errs[ipar]
+    #        pars.append(par)
+    #        parerrs.append(parerr)
+    #        print('minimizeChi2 fit result: par {}: {} +/- {}'.format(parname, par, parerr))
+    #else:
+    #    print('minimizeLhood :: FAILED MINIMIZATION! :-(')
+
+    return pars, parerrs
 
 
-#################################
+##################################################################
 # support both 1D and 2D versions on demand
-def fitSignalStrength(hsig, hbg, hdata, is1D, step = 0.01, mumin = 0., mumax = 2.):
+def doTheFit(grs, step = 0.01):
 
-    del gsigVals[:]
-    del gbgVals[:]
-    del gdataVals[:]
-    
-    # using lists as storage ;-)
-    # 1D:
-    if is1D:
-        for i in range(1, hdata.GetNbinsX()+1):
-            gsigVals.append(hsig.GetBinContent(i))
-            gbgVals.append(hbg.GetBinContent(i))
-            gdataVals.append(hdata.GetBinContent(i))
-    else:
-        # 2D:
-        for i in range(1, hdata.GetNbinsX()+1):
-            for j in range(1, hdata.GetNbinsY()+1):
-                gsigVals.append(hsig.GetBinContent(i,j))
-                gbgVals.append(hbg.GetBinContent(i,j))
-                gdataVals.append(hdata.GetBinContent(i,j))
-            
-    mu, muerr = minimizeLhood(step, mumin, mumax)
-    return mu, muerr
+    gInitPars = {}
+    #del gPars[:]
 
-#################################
-def ComputeZeroCompatibility(mu, muerr):
-    #hmu = dummmodel.Clone('muh')
-    #hmu.Reset()
-    #hmu.SetBinContent(1, mu)
-    #hmu.SetBinError(1, muerr)
-    #p0 = hmu.Chi2Test(dummmodel, 'WW') # by default returns the pval
+    initGlobalPars()
+    npars = prepareData(grs)
+    print('Will fit data points:')
+    for region in gDataPoints:
+        print(f'  Region {region}')
+        for dpoint in gDataPoints[region]:
+            print('    beta={} y={} ey={}'.format(dpoint.x, dpoint.y, dpoint.ey))
+    pars, parerrs = minimizeChi2(npars, step)
+    return pars, parerrs
+
+##################################################################
+def ComputeZeroCompatibility(val, sigma):
     p0 = kBadP0
-    if muerr > 0:
-        p0 = ROOT.TMath.Prob(pow(mu/muerr, 2), 1)
-    
+    if sigma > 0:
+        p0 = ROOT.TMath.Prob(pow(val/sigma, 2), 1)
     t0 = 999.
     logt0 = 999.
     if p0 > 0:
@@ -137,6 +332,6 @@ def ComputeZeroCompatibility(mu, muerr):
             logt0 = log(t0)
     return p0, logt0
             
-#################################
-#################################
-#################################
+##################################################################
+##################################################################
+##################################################################
